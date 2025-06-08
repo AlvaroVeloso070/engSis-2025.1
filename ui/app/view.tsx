a80c43b75200d46ac0944aced791ed33c1731efd
Thiago Telho <ttelho@gmail.com>
2025-06-08 18:18:15 -0300

    feat(ui): implement soil and barometric data analysis with corresponding UI components

--- ui/app/view.tsx
+++ ui/app/view.tsx
@@ -15,6 +15,7 @@ import DataCard from "./components/DataCard";
 import Button from "./components/Button";
 import TopBar from "./components/TopBar";
 import { start } from "repl";
+import { motion } from "framer-motion";
 
 const sideBarItems = [
   {
@@ -65,6 +66,15 @@ export type AltimetryData = {
   lowestPoint: number;
 };
 
+export type SoilData = {
+  soilMoisture: number;
+  soilType: SoilType;
+};
+
+export type BarometricData = {
+  hPa: number;
+};
+
 enum SoilType {
   SILTY = "SILTY",
   SANDY = "SANDY",
@@ -81,6 +91,13 @@ export default function MainView() {
   const [altimetryData, setAltimetryData] = useState<AltimetryData | null>(
     null
   );
+
+  const [soilData, setSoilData] = useState<SoilData | null>(null);
+
+  const [barometricData, setBarometricData] = useState<BarometricData | null>(
+    null
+  );
+
   async function requestTrackingData() {
     fetch(`${process.env.NEXT_PUBLIC_API_URL}/altimetry`)
       .then((res) => res.json())
@@ -88,7 +105,73 @@ export default function MainView() {
       .catch((err) => console.error(err));
   }
 
+  async function requestSoilData() {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/soil`)
+      .then((res) => res.json())
+      .then((json) =>
+        setSoilData({
+          soilMoisture: json.soilMoisture,
+          soilType:
+            json.soilType === "SILTY"
+              ? SoilType.SILTY
+              : json.soilType === "SANDY"
+              ? SoilType.SANDY
+              : json.soilType === "ROCKY"
+              ? SoilType.ROCKY
+              : SoilType.CLAY,
+        })
+      )
+      .catch((err) => console.error(err));
+    setTimeout(() => {
+      setStartedSoilAnalysis(false);
+    }, 5000);
+  }
+
+  async function requestBarometricData() {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/barometry`)
+      .then((res) => res.json())
+      .then((json) =>
+        setBarometricData({
+          hPa: json.hPa,
+        })
+      )
+      .catch((err) => console.error(err));
+    setTimeout(() => {
+      setStartedBarometricAnalysis(false);
+    }, 5000);
+  }
+
+  async function requestViabilityData() {
+    fetch(`${process.env.NEXT_PUBLIC_API_URL}/viability`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        totalLandArea: altimetryData?.totalLandArea ?? 0,
+        highestPoint: altimetryData?.highestPoint ?? 0,
+        lowestPoint: altimetryData?.lowestPoint ?? 0,
+        soilMoisture: soilData?.soilMoisture ?? 0,
+        soilType: soilData?.soilType ?? SoilType.SILTY,
+        hPa: barometricData?.hPa ?? 0,
+      }),
+    })
+      .then((res) => res.json())
+      .then((json) =>
+        setViability({
+          totalCost: json.totalCost,
+          costPerM2: json.costPerM2,
+        })
+      )
+      .catch((err) => console.error(err));
+  }
+
   const [startedTracking, setStartedTracking] = useState(false);
+
+  const [startedSoilAnalysis, setStartedSoilAnalysis] = useState(false);
+
+  const [startedBarometricAnalysis, setStartedBarometricAnalysis] =
+    useState(false);
 
   return (
     <section className="flex flex-col h-screen">
@@ -117,9 +200,26 @@ export default function MainView() {
                   Projeto de Engenharia de Sistemas
                 </p>
               </div>
-              <h5 className="self-stretch col-span-1 text-displayxs text-label-light-primary font-semibold font-inter ">
-                Análise de solo
-              </h5>
+              <div className="flex items-center justify-between w-full">
+                <h5 className="self-stretch col-span-1 text-displayxs text-label-light-primary font-semibold font-inter ">
+                  Análise de solo
+                </h5>
+                <Button
+                  label="Calcular viabilidade"
+                  disabled={
+                    altimetryData === null ||
+                    soilData === null ||
+                    barometricData === null
+                  }
+                  onClick={() => {
+                    requestViabilityData();
+                  }}
+                  iconPosition="trailing"
+                  size="xs"
+                  presetStyle="brand"
+                  icon={{ glyph: Glyph.ArrowUp, size: IconSize.Small }}
+                />
+              </div>
             </div>
           </header>
           <main className="flex-1 flex min-w-0 h-full  items-start  overflow-x-hidden ">
@@ -129,7 +229,7 @@ export default function MainView() {
                   <DataCard
                     className="flex-1"
                     title="Área total do terreno"
-                    data={1000}
+                    data={altimetryData?.totalLandArea ?? 0}
                     unit="m²"
                     unitPlacement="end"
                   />
@@ -158,7 +258,7 @@ export default function MainView() {
                     <DataCard
                       className="flex-1"
                       title="Umidade do solo"
-                      data={1000}
+                      data={soilData?.soilMoisture ?? 0}
                       unit="%"
                       unitPlacement="end"
                     />
@@ -280,13 +380,260 @@ export default function MainView() {
                         iconPosition="trailing"
                         size="sm"
                         presetStyle="brand"
-                        icon={{ glyph: Glyph.Add, size: IconSize.Small }}
+                        icon={{ glyph: Glyph.Checkmark, size: IconSize.Small }}
                       />
                     )}
                   </div>
                 </div>
-                <div className="flex flex-1 h-full flex-col px-spacing-xs py-spacing-sm border-r-[1px] border-levels-200 border-dashed"></div>
-                <div className="flex flex-1 h-full flex-col px-spacing-xs py-spacing-sm"></div>
+                <div className="flex flex-1 h-full flex-col py-spacing-sm border-r-[1px] border-levels-200 border-dashed px-spacing-xl gap-y-spacing-lg">
+                  <div className="flex flex-col bg-levels-25 border-[1px] border-levels-100 rounded-lg h-fit px-spacing-md py-spacing-sm gap-y-spacing-sm">
+                    {startedSoilAnalysis ? (
+                      <LoadingCircle />
+                    ) : soilData === null ? (
+                      <>
+                        <p className="text-textsm font-regular text-label-light-primary">
+                          Análise de tipo de solo.
+                        </p>
+                        <p className="text-textxs font-regular text-label-light-secondary">
+                          Para realizar a análise de solo, é necessário
+                          primeiramente identificar o tipo de solo presente no
+                          terreno. Para isso, retire uma pequena amostra de solo
+                          e posicione no sensor na parte inferior do
+                          dispositivo. O sensor irá identificar o tipo de solo e
+                          exibir na tela. Abaixo, você pode ver o tipo de solo
+                          identificado e as informações relacionadas.
+                        </p>
+                        <div className="flex items-center justify-center mt-[-20px]">
+                          <svg
+                            width="300"
+                            height="200"
+                            viewBox="0 0 400 200"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <path
+                              d="M0 200 Q100 60, 200 120 Q300 180, 400 100 L400 200 Z"
+                              fill="rgb(60, 60, 67, 40%)"
+                              stroke="rgb(60, 60, 67, 70%)"
+                              strokeWidth="2"
+                              fillOpacity="0.3"
+                            />
+                          </svg>
+                        </div>
+                      </>
+                    ) : (
+                      <div className="flex flex-col gap-y-spacing-md">
+                        <DataCard
+                          className="flex-1"
+                          title="Tipo de solo"
+                          data={
+                            soilData.soilType === SoilType.SILTY
+                              ? "Siltoso"
+                              : soilData.soilType === SoilType.SANDY
+                              ? "Arenoso"
+                              : soilData.soilType === SoilType.ROCKY
+                              ? "Pedregoso"
+                              : "Argiloso"
+                          }
+                          unit={""}
+                          unitPlacement="end"
+                        />
+                        <DataCard
+                          className="flex-1"
+                          title="Umidade do solo"
+                          data={soilData.soilMoisture}
+                          unit="%"
+                          unitPlacement="end"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex flex-col bg-levels-25 border-[1px] border-levels-100 rounded-lg h-fit px-spacing-md py-spacing-sm gap-y-spacing-sm items-center">
+                    {!startedSoilAnalysis && soilData === null ? (
+                      <Button
+                        label="Iniciar análise de solo"
+                        onClick={() => {
+                          setStartedSoilAnalysis(true);
+                          requestSoilData();
+                        }}
+                        iconPosition="trailing"
+                        size="sm"
+                        presetStyle="floating"
+                        icon={{ glyph: Glyph.Add, size: IconSize.Small }}
+                      />
+                    ) : !startedSoilAnalysis && soilData !== null ? (
+                      <Button
+                        label="Limpar dados"
+                        onClick={() => setSoilData(null)}
+                        iconPosition="trailing"
+                        size="sm"
+                        presetStyle="dangerous"
+                        icon={{
+                          glyph: Glyph.Delete,
+                          size: IconSize.Small,
+                          primaryColor: "fill-white",
+                        }}
+                      />
+                    ) : (
+                      <p className="text-textsm font-regular text-label-light-primary">
+                        Análise de solo em andamento...
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <div className="flex flex-1 h-full flex-col  py-spacing-sm px-spacing-xl gap-y-spacing-lg">
+                  <div className="flex flex-col bg-levels-25 border-[1px] border-levels-100 rounded-lg h-fit px-spacing-md py-spacing-sm gap-y-spacing-sm">
+                    {startedBarometricAnalysis ? (
+                      <LoadingCircle />
+                    ) : barometricData === null ? (
+                      <>
+                        <p className="text-textsm font-regular text-label-light-primary">
+                          Levantamento de área e altimetria.
+                        </p>
+                        <p className="text-textxs font-regular text-label-light-secondary">
+                          Para realizar a análise de solo, é necessário o
+                          levantamento da área total do terreno e a altimetria
+                          do local. Essas informações são essenciais para
+                          determinar a viabilidade da obra e os custos
+                          envolvidos. A baixo, cliquei no botão rastrear caminho
+                          e percorra o perímetro do terreno, clicando nos pontos
+                          de interesse. Após finalizar o percurso, clique em
+                          "Finalizar" para calcular a área total do terreno e a
+                          altimetria do local.
+                        </p>
+                        <div className="flex items-center justify-center">
+                          <svg
+                            width="100"
+                            height="300"
+                            viewBox="0 0 100 300"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <rect
+                              x="40"
+                              y="20"
+                              width="20"
+                              height="200"
+                              rx="10"
+                              ry="10"
+                              fill="#ccc"
+                              stroke="#555"
+                              strokeWidth="2"
+                            />
+
+                            <rect
+                              x="45"
+                              y="100"
+                              width="10"
+                              height="120"
+                              fill="rgb(60, 60, 67, 70%)"
+                            />
+
+                            <circle
+                              cx="50"
+                              cy="240"
+                              r="20"
+                              fill="rgb(60, 60, 67, 70%)"
+                              stroke="#555"
+                              strokeWidth="2"
+                            />
+
+                            <circle
+                              cx="50"
+                              cy="240"
+                              r="22"
+                              fill="none"
+                              stroke="#555"
+                              strokeWidth="2"
+                            />
+
+                            <line
+                              x1="30"
+                              y1="40"
+                              x2="40"
+                              y2="40"
+                              stroke="#333"
+                              strokeWidth="2"
+                            />
+                            <line
+                              x1="30"
+                              y1="80"
+                              x2="40"
+                              y2="80"
+                              stroke="#333"
+                              strokeWidth="2"
+                            />
+                            <line
+                              x1="30"
+                              y1="120"
+                              x2="40"
+                              y2="120"
+                              stroke="#333"
+                              strokeWidth="2"
+                            />
+                            <line
+                              x1="30"
+                              y1="160"
+                              x2="40"
+                              y2="160"
+                              stroke="#333"
+                              strokeWidth="2"
+                            />
+                            <line
+                              x1="30"
+                              y1="200"
+                              x2="40"
+                              y2="200"
+                              stroke="#333"
+                              strokeWidth="2"
+                            />
+                          </svg>
+                        </div>
+                      </>
+                    ) : (
+                      <div className="flex flex-col gap-y-spacing-md">
+                        <DataCard
+                          className="flex-1"
+                          title="Pressão atmosférica"
+                          data={barometricData.hPa}
+                          unit="hPa"
+                          unitPlacement="end"
+                        />
+                      </div>
+                    )}
+                  </div>
+                  <div className="flex flex-col bg-levels-25 border-[1px] border-levels-100 rounded-lg h-fit px-spacing-md py-spacing-sm gap-y-spacing-sm items-center">
+                    {!startedBarometricAnalysis && barometricData === null ? (
+                      <Button
+                        label="Iniciar análise de pressão atmosférica"
+                        onClick={() => {
+                          setStartedBarometricAnalysis(true);
+                          requestBarometricData();
+                        }}
+                        iconPosition="trailing"
+                        size="sm"
+                        presetStyle="floating"
+                        icon={{ glyph: Glyph.Add, size: IconSize.Small }}
+                      />
+                    ) : !startedBarometricAnalysis &&
+                      barometricData !== null ? (
+                      <Button
+                        label="Limpar dados"
+                        onClick={() => setBarometricData(null)}
+                        iconPosition="trailing"
+                        size="sm"
+                        presetStyle="dangerous"
+                        icon={{
+                          glyph: Glyph.Delete,
+                          size: IconSize.Small,
+                          primaryColor: "fill-white",
+                        }}
+                      />
+                    ) : (
+                      <p className="text-textsm font-regular text-label-light-primary">
+                        Análise de pressão em andamento...
+                      </p>
+                    )}
+                  </div>
+                </div>
               </div>
             </div>
           </main>
@@ -355,5 +702,31 @@ function TrackingLoadingAnimation() {
         />
       </LineChart>
     </ResponsiveContainer>
+  );
+}
+
+export function LoadingCircle() {
+  const circumference = 2 * Math.PI * 45;
+
+  return (
+    <div className="flex justify-center items-center h-48">
+      <svg className="w-24 h-24" viewBox="0 0 100 100" fill="none">
+        <circle cx="50" cy="50" r="45" stroke="#e5e7eb" strokeWidth="10" />
+
+        <motion.circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="#ff004d"
+          strokeWidth="10"
+          strokeLinecap="round"
+          fill="transparent"
+          strokeDasharray={circumference}
+          strokeDashoffset={circumference}
+          animate={{ strokeDashoffset: 0 }}
+          transition={{ duration: 5, ease: "linear" }}
+        />
+      </svg>
+    </div>
   );
 }
